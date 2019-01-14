b5788145716ca30716a1cfbbed027c625fdbfdd9
Sean B. Higgins <higgisea@oregonstate.edu>
2019-01-13 21:04:58 -0800

    Furtur detailing the function prerequesits, and how to test to see if the entered matricies are valid for the given function.

--- Matrix.sh
+++ Matrix.sh
@@ -7,3 +7,26 @@
 #													(Important: The two matricies MUST have the same dimenstions)
 #		matrix multiply LEFT_MATRIX RIGHT_MATRIX	(exactily what it says, multiplys two matricies together and returns the result)
 #													(Important: The number of rows of the first matrix MUST equal the number of collumns of the second matrix)
+
+# matrix dims [MATRIX]
+def dims(MATRIX):
+
+# matrix transpose [MATRIX]
+# Call the dims function to find the number of rows and collumns of the matrix
+
+
+# matrix mean [MATRIX]
+# Call the dims function to find the number of rows and collumns of the matrix
+# Use the obtained values to add together the elements of each collumn.
+
+
+# matrix add LEFT_MATRIX RIGHT_MATRIX
+# Call the dims function to find the number of rows and collumns of each matrix
+# Compare the values, and make sure each matrix has the same dimensions.
+
+
+# matrix multiply LEFT_MATRIX RIGHT_MATRIX
+# Call the dims function to find the number of rows and collumns of each matrix.
+# Compare the number of rows of the LEFT_MATRIX to the number of collumns of the RIGHT_MATRIX.
+# They absolutely MUST be equal. The resulting matrix will have the same number of rows the LEFT_MATRIX has as collumns,
+# and the same number of collumns as the RIGHT_MATRIX has as rows.
