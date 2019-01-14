fec0456b8fd7feac9d75015bd3c3f5f9ea8bfc12
Sean B. Higgins <higgisea@oregonstate.edu>
2019-01-14 12:07:01 -0800

    Providing more details as to what the error codes represent, and what each function is looking for in terms of errors.

--- Matrix.sh
+++ Matrix.sh
@@ -9,10 +9,14 @@
 #													(Important: The number of rows of the first matrix MUST equal the number of collumns of the second matrix)
 
 # matrix dims [MATRIX]
-def dims(MATRIX):
+# Checks to see if the values in the matrix are all signed integers. (Error Code 1)
+# Checks to see if the number of elements in each row is consistant throughout the matrix (i.e. each collumn is filled out) (Error Code 2)
+
+
 
 # matrix transpose [MATRIX]
 # Call the dims function to find the number of rows and collumns of the matrix
+
 
 
 # matrix mean [MATRIX]
@@ -20,9 +24,11 @@ def dims(MATRIX):
 # Use the obtained values to add together the elements of each collumn.
 
 
+
 # matrix add LEFT_MATRIX RIGHT_MATRIX
 # Call the dims function to find the number of rows and collumns of each matrix
 # Compare the values, and make sure each matrix has the same dimensions.
+# If the matricies are different in size, return an error. (Error Code 3)
 
 
 # matrix multiply LEFT_MATRIX RIGHT_MATRIX
@@ -30,3 +36,4 @@ def dims(MATRIX):
 # Compare the number of rows of the LEFT_MATRIX to the number of collumns of the RIGHT_MATRIX.
 # They absolutely MUST be equal. The resulting matrix will have the same number of rows the LEFT_MATRIX has as collumns,
 # and the same number of collumns as the RIGHT_MATRIX has as rows.
+# If these conditions are not met, return an error code. (Error Code 4)
