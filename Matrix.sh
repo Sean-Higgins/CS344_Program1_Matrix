d9762ecc8b0421c4819530f11f355a8b828c725d
Sean B. Higgins <higgisea@oregonstate.edu>
2019-01-16 13:31:20 -0800

    Working on the first function, the dims function. Made good progress, but further testing needed. The function can read rows of data and elements from those rows, but enters into an infinite loop before going to the next row. That needs to be addressed. Also created a test page for the expected outputs of various operations on the two matrix files I have. I'll add more to it later.

--- Matrix.sh
+++ Matrix.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#!/bin/bash -e
 # This program performs a number of matrix operations:
 #		matrix dims [MATRIX]		(for finding the dimenstions of the matrix, also pulls double duty as a verification test)
 #		matrix transpose [MATRIX]	(swaps the rows for the collums and visa-versa)
@@ -7,10 +7,85 @@
 #													(Important: The two matricies MUST have the same dimenstions)
 #		matrix multiply LEFT_MATRIX RIGHT_MATRIX	(exactily what it says, multiplys two matricies together and returns the result)
 #													(Important: The number of rows of the first matrix MUST equal the number of collumns of the second matrix)
+# Note: The -e flag by the shabang (i.e. the #!/bin/bash line) makes sure the program stops on an error, an exit code of anything other than 0.
+# Note: The -x flag by the shabang prints the program line by line as it exicutes, useful for debugging purposes.
 
 # matrix dims [MATRIX]
 # Checks to see if the values in the matrix are all signed integers. (Error Code 1)
 # Checks to see if the number of elements in each row is consistant throughout the matrix (i.e. each collumn is filled out) (Error Code 2)
+numOfRows=0
+numOfCols=0
+oldNumCols=0
+
+rowData=""
+elemData=""
+
+errCode=0
+
+result=""
+
+matrixFile=./matrix1.txt
+
+
+echo "Beginning to read the matrix file."
+echo "numOfRows: $numOfRows"
+echo "numOfCols: $numOfCols"
+echo "errCode: $errCode"
+echo "rowData: $rowData"
+echo "elemData: $elemData"
+echo "result: $result"
+
+while read rowData < $matrixFile
+do
+
+	oldNumCols=numOfCols
+
+	echo "Reading Rows"
+	echo "numOfRows: $numOfRows"
+	echo "numOfCols: $numOfCols"
+	echo "errCode: $errCode"
+	echo "rowData: $rowData"
+	echo "elemData: $elemData"
+	echo "result: $result"
+
+	for elem in $rowData
+	do
+	
+		echo "In Element Processing Loop"
+		echo "i.e. Collumn Counting and Verifying Loop"
+		echo "numOfRows: $numOfRows"
+		echo "numOfCols: $numOfCols"
+		echo "errCode: $errCode"
+		echo "rowData: $rowData"
+		echo "elemData: $elemData"
+		echo "result: $result"
+		
+		echo "elem: $elem"
+		numOfCols=$(expr $numOfCols + 1 )
+		echo "collumns: $numOfCols"
+		echo -e "End of Collumn Counting Loop"
+	done
+	
+	if [ "$numOfCols" -eq 0 ]
+	then
+		errCode=1
+		echo "Error Code $errCode: Matrix empty." 1>&2
+		exit errCode
+	elif [ "$numOfCols" -ne "$oldNumCols" ]
+	then
+		errCode=2
+		echo "Error Code $errCode: Matrix incorrectly constructed." 1>&2
+		exit errCode
+	else
+	
+		numOfRows=$(expr $numOfRows + 1)
+	fi
+	done
+	
+	echo "$numOfRows $numOfCols" > dimsResults
+	
+done < $dimsResults
+	
 
 
 
